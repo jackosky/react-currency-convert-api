0102f9e394a7fbaeb52672710e84ec6c4eac6ae3
Jacek <jacek.poreda@sonarsource.com>
2021-10-31 14:10:02 +0100

    Initial commit - create app form template

---
+++ src/main/java/com/jackosky/currency/CurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.jackosky.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyApplication.class, args);
+	}
+
+}
